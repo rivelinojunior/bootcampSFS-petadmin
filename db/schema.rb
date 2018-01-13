d7f53df40144946ea09a82910eb233295b14bb42
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-01-13 17:18:35 -0400

    generate resource fae (client)

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180113202744) do
+ActiveRecord::Schema.define(version: 20180113211634) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "clients", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.text     "observations"
+    t.string   "phone"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "fae_changes", force: :cascade do |t|
     t.integer  "changeable_id"
