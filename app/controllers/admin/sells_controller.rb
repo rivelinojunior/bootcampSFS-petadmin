3f367b8fabac60212816b85848c037febfcd5c08
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-01-18 17:57:24 -0400

    add callback in sells_controller to convert status in integer

--- app/controllers/admin/sells_controller.rb
+++ app/controllers/admin/sells_controller.rb
@@ -1,5 +1,12 @@
 module Admin
   class SellsController < Fae::BaseController
+    before_action :adjust_enum, only: [:create, :update]
 
+    
+    private 
+      
+    def adjust_enum
+      params[:sell][:status] = params[:sell][:status].to_i
+    end
   end
 end
