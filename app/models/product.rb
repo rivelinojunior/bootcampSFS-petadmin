d7fbbe02ff9df38b2b8d1f28e910ccff57eb6b0a
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-01-18 20:13:29 -0400

    add validations to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,14 @@
 class Product < ApplicationRecord
   include Fae::BaseModelConcern
 
+  validates :title, uniqueness: true, presence: true
+  validates :price, presence: true
+  
+  has_fae_image :hero_image
+  belongs_to :supplier
+  
   def fae_display_field
     title
   end
 
-  has_fae_image :hero_image
-
-
-  belongs_to :supplier
 end
