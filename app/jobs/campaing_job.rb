1a47baea9a9b5bc088f4e4c600d5e57751d2db4f
Rivelino Junior <rv.fernandes.junior@gmail.com>
2018-01-19 02:34:39 -0400

    call JobCampaing in callback

--- app/jobs/campaing_job.rb
+++ app/jobs/campaing_job.rb
@@ -1,7 +1,8 @@
 class CampaingJob < ApplicationJob
   queue_as :emails
  
-  def perform(client, title, body)
+  def perform(client, title, body, campaign_id)
+    CampaignClient.create(campaign_id: campaign_id, client: client)
     MarketingMailer.campaign(client, title, body).deliver_now
   end
 end
--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -7,9 +7,17 @@ class Campaign < ApplicationRecord
   has_many :campaign_clients
   has_many :clients, through: :campaign_clients
   
+  after_save :schedule_emails
+
   def fae_display_field
     title
   end
 
-
+  private
+  
+  def schedule_emails
+    Client.all.each do |client|
+      CampaingJob.perform_later client, self.title, self.body, self.id
+    end
+  end
 end
